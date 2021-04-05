69e6d80e165ec8bd67831de7f5257a9942367248
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-05 20:44:44 +0530

    Edit existing contacts person using their name

--- src/main/java/com/addressbook/bridgelabz/AddressBook.java
+++ src/main/java/com/addressbook/bridgelabz/AddressBook.java
@@ -9,8 +9,8 @@ import java.util.Scanner;
  */
 public class AddressBook {
     Contacts contacts;
+    static Scanner scanner = new Scanner(System.in);
     private void addContacts() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("enter the first name");
         String firstName = scanner.nextLine();
         System.out.println("enter the last name");
@@ -31,6 +31,37 @@ public class AddressBook {
         contacts = new Contacts(firstName, lastName, address, state, city, zipCode, number, email);
         System.out.println("contact successfully added");
     }
+    /* @Description - in this we edit contacts details  */
+
+    public void editContactDetails(String firstName) {
+        if (contacts == null)
+            System.out.println("Address book is empty");
+        else {
+            if (contacts.getFirstName().equalsIgnoreCase(firstName)) {
+                System.out.println("edit the contact details");
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("enter the first name");
+                contacts.setFirstName(scanner.next());
+                System.out.println("enter the last name");
+                contacts.setLastName(scanner.next());
+                System.out.println("enter address");
+                contacts.setAddress(scanner.next());
+                System.out.println("enter the state");
+                contacts.setState(scanner.next());
+                System.out.println("enter the city");
+                contacts.setCity(scanner.next());
+                System.out.println("enter the zipcode");
+                contacts.setZip(scanner.nextInt());
+                System.out.println("enter mobile number");
+                contacts.setMobileNumber(scanner.nextLong());
+                System.out.println("enter the email-id");
+                contacts.setEmailId(scanner.next());
+
+                System.out.println("Contact successfully updated");
+            } else
+                System.out.println("contact not found");
+        }
+    }
     @Override
     public String toString(){
         return contacts.toString();
@@ -39,6 +70,8 @@ public class AddressBook {
     public static void main(String[] args) {
         AddressBook addressBook = new AddressBook();
         addressBook.addContacts();
+        System.out.println("enter the first name of contact you wanna  edit");
+        addressBook.editContactDetails(scanner.next());
         System.out.println(addressBook.toString());
     }
 }

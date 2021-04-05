9e6b10d483450ab51afda5ae7b00a65040ff4570
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-05 13:33:53 +0530

    Added Contact details in address book system

--- src/main/java/com/addressbook/bridgelabz/AddressBook.java
+++ src/main/java/com/addressbook/bridgelabz/AddressBook.java
@@ -1,9 +1,14 @@
 package com.addressbook.bridgelabz;
-
+/*
+@Description - To create a contacts in address book with first name, last name, address, city, state,
+* zip,mobile number.
+ */
 public class AddressBook {
     public static void main(String[] args) {
-        System.out.println("Welcome to address book system program");
+        Contacts contact = new Contacts("Rudra", "Prajapati", "Old madivala",
+                "Benglore", "Karnataka", 560068,
+                91805644558L, "rudra@gmail.com");
+        System.out.println(contact.toString());
     }
-
 
 }

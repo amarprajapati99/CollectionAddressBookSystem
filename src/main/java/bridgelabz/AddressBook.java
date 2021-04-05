8be37633ab7c2e42a097ddaa50f4db6636eeb83a
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-05 12:20:20 +0530

    Refactor: move class test to main java

--- src/main/java/bridgelabz/AddressBook.java
+++ src/main/java/bridgelabz/AddressBook.java
@@ -1,8 +1,9 @@
-package com.addressbooksystem.bridgelabz;
+package bridgelabz;
 
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to address book system program");
     }
+
 
 }
